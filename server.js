dca6de183247e7d953fbb67dbd05b0408ef04f7b
niteshpal9619 <mssnv42@gmail.com>
2024-05-12 06:02:25 +0530

    Inside the server.js database connection is added

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ mongoose.connect(DB_URL, {
     useUnifiedTopology: true,
     useFindAndModify: false,
 });
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
